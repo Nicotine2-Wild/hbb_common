c34f843370dbb7fcae93d39ba4938cc2e280c135
Nicotine2-Wild <nicotine2.wild@gmail.com>
2025-04-10 17:03:50 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.nicotine-2.com"];
-pub const RS_PUB_KEY: &str = "qwertyuioplkjhgfdsa=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xxxxxxx"];
+pub const RS_PUB_KEY: &str = "xxxxx";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
